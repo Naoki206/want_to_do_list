bd617c3ebbfbe774f2d6c798ae01399f90789cab
Naoki206 <naoki020602060206@gmail.com>
2020-03-12 13:15:49 +0900

    レイアウト作成、適応

--- resources/views/folders/create.blade.php
+++ resources/views/folders/create.blade.php
@@ -1,44 +1,32 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>ToDo App</title>
-  <link rel="stylesheet" href="/css/styles.css">
-</head>
-<body>
-  <header>
-    <nav class="my-navbar">
-      <a class="my-navbar-brand" href="/">ToDo App</a>
-    </nav>
-  </header>
-  <main>
-    <div class="container">
-      <div class="row">
-        <div class="col col-md-offset-3 col-md-6">
-          <nav class="panel panel-default">
-            <div class="panel-heading">
-            <a href="{{ route('folders.create') }}" class="btn btn-default btn-block">
-                項目を追加する
-            </a>
-            </div>
-            <div class="panel-body">
-              <form action="{{ route('folders.create') }}" method="post">
-                @csrf
-                <div class="form-group">
-                  <label for="title">項目名</label>
-                  <input type="text" class="form-control" name="title" id="title" />
-                </div>
-                <div class="text-right">
-                  <button type="submit" class="btn btn-primary">追加</button>
-                </div>
-              </form>
-            </div>
-          </nav>
-        </div>
+@extends('layout')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col col-md-offset-3 col-md-6">
+        <nav class="panel panel-default">
+          <div class="panel-heading">項目を追加する</div>
+          <div class="panel-body">
+            @if($errors->any())
+              <div class="alert alert-danger">
+                @foreach($errors->all() as $message)
+                  <p>{{ $message }}</p>
+                @endforeach
+              </div>
+            @endif
+            <form action="{{ route('folders.create') }}" method="post">
+              @csrf
+              <div class="form-group">
+                <label for="title">項目名</label>
+                <input type="text" class="form-control" name="title" id="title" value="{{ old('title') }}" />
+              </div>
+              <div class="text-right">
+                <button type="submit" class="btn btn-primary">追加</button>
+              </div>
+            </form>
+          </div>
+        </nav>
       </div>
     </div>
-  </main>
-</body>
-</html>
+  </div>
+@endsection
