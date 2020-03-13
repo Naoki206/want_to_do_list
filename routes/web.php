eac96aecb84f2291ba3de36309fa81558923b860
Naoki206 <naoki020602060206@gmail.com>
2020-03-13 09:48:00 +0900

    toppage 作成

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,6 @@ Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name(
 Route::post('/folders/{id}/tasks/create', 'TaskController@create');
 Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
 Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit')->name('tasks.edit_confirm');
+Route::get('/', 'HomeController@index')->name('home');
+
 
