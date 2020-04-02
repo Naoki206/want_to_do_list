c648b63253bdf2f5638cbba40f28d1c08b5fc036
Naoki206 <naoki020602060206@gmail.com>
2020-04-02 23:54:24 +0900

    root 元に戻す

--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,8 @@ Route::group(['middleware' => 'auth'], function() {
     
     Route::group(['middleware' => 'can:view,folder'], function() {
         Route::get('/folders/{folder}/tasks', 'TaskController@index')->name('tasks.index');
-
         Route::get('/folders/{folder}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
         Route::post('/folders/{folder}/tasks/create', 'TaskController@create');
-
         Route::get('/folders/{folder}/tasks/{task}/edit', 'TaskController@showEditForm')->name('tasks.edit');
         Route::post('/folders/{folder}/tasks/{task}/edit', 'TaskController@edit');
     });
