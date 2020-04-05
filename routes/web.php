0ae18b83bd991b8adcddfa8fa0c799dd2356000b
Naoki206 <naoki020602060206@gmail.com>
2020-04-05 21:22:09 +0900

    TODO 追加

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Route::group(['middleware' => 'auth'], function() {
     Route::post('/folders/create', 'FolderController@create');
     
     Route::group(['middleware' => 'can:view,folder'], function() {
+        // TODO: 削除機能追加
         Route::get('/folders/{folder}/tasks', 'TaskController@index')->name('tasks.index');
         Route::get('/folders/{folder}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
         Route::post('/folders/{folder}/tasks/create', 'TaskController@create');
